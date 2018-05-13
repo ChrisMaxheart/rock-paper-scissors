d23e441a589804a53434d21720d560e4741a8fa7
Samuel Henry Kurniawan <samuelhenrykurniawan@yahoo.com>
2018-05-14 00:56:05 +0800

    full version update

---
+++ script/script.js
@@ -0,0 +1,98 @@
+function computerPlay(rockCounter, paperCounter, scissorsCounter) {
+  let a = rockCounter;
+  let b = paperCounter;
+  let c = scissorsCounter;
+  if (a+b+c == 0) {
+    return "paper";
+  }
+  let tot = a + b +c;
+  a /= tot;
+  b /= tot;
+  c /= tot;
+  let ans = Math.random();
+  if (ans < a) {
+    return "paper";
+  } else if (ans < (a+b)) {
+    return "scissors";
+  } else {
+    return "rock";
+  }
+}
+
+function playRound(player, computer) {
+  if (player == computer) {
+    return "Draw!";
+  } else if ((player == "rock" && computer == "scissors") || (player == "scissors" && computer == "paper") || (player == "paper" && computer == "rock")) {
+    return "You Win!";
+  } else {
+    return "You Lose!";
+  }
+}
+
+
+let a = 0;
+let b = 0;
+let c = 0;
+let hum = 0;
+let cpu = 0;
+let draw = 0;
+
+function start(rockCounter, paperCounter, scissorsCounter) {
+  a = rockCounter;
+  b = paperCounter;
+  c = scissorsCounter;
+  hum = 0;
+  cpu = 0;
+  draw = 0;
+  alert("Let the Torture Begin!");
+  return;
+}
+
+
+function clicks(choose) {
+  let com = computerPlay(a, b, c);
+  let res = playRound(choose, com);
+  let msg = document.getElementById("result");
+  if (choose == "rock") {
+    a++;
+  } else if (choose == "paper") {
+    b++;
+  } else {
+    c++;
+  }
+  if(res == "You Win!") {
+    hum++
+  } else if (res == "You Lose!") {
+    cpu++;
+  } else {
+    draw++;
+  }
+  result.innerHTML = res;
+  document.getElementById("myscore").innerHTML = hum;
+  document.getElementById("oppscore").innerHTML = cpu;
+  document.getElementById("drawscore").innerHTML = draw;
+  if (hum == 11) {
+    alert("You won my AI! Congrats!");
+  } else if (cpu == 11) {
+    alert("Wah so weak!");
+  }
+  if (hum == 11 || cpu == 11) {
+    alert("The game will be restarted using a better AI.");
+    start(a, b, c);
+  }
+  return;
+}
+
+
+
+document.onload = start(0, 0, 0);
+
+
+let rck = document.getElementById("rock");
+rck.addEventListener('click', function() {clicks("rock")});
+
+let ppr = document.getElementById("paper");
+let sci = document.getElementById("scissors");
+
+ppr.addEventListener('click', function() {clicks("paper")});
+sci.addEventListener("click", function() {clicks("scissors")});
